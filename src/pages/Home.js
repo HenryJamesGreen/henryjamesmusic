1d4ce2d5cb0db8cee0acd137d3d914d2ba6d779e
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-04-10 19:50:40 +0100

    added navbar and video to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 /* eslint-disable */
-
+import YoutubeEmbed from "../components/YouTubeHome";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 //audio
 function Home() {
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Card sx={{ minWidth: 200 , bgcolor: "black" }}>
+      <CardContent>
+    <YoutubeEmbed embedId="8p8rHgOCu7A" />
+    </CardContent>
+    </Card>
   );
 }
 
