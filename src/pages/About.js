bd49affb38d4515965f63d0ebdd707346265a0a7
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-04-10 18:17:26 +0100

    added nav bar sample

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+
+//audio
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ContactForm />
+      </header>
+    </div>
+  );
+}
+
+export default About;
